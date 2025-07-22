26db83aeb8b5d78b22dd0d49500d0341ffa977fa
MelissaNunes10 <RAL-MelissaN@fedextest.onmicrosoft.com>
2025-07-22 14:58:30 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,7 @@ function App() {
           <Route path="/example-graph" element={<ExampleGraphPage />} />
           <Route path="/example-table" element={<ExampleTablePage />} />
           <Route path="/heat-vs-size-graph" element={<HeatVsSizeGraph />} />
-          <Route path="/overview-table" element={<OverviewTablePage />} />
+          <Route path="/overview-table" element={<OverviewPage />} />
         </Routes>
       </Router>
     {/* Show the footer at the bottom of every page */}
